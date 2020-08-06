d19e37b6146b77cfb5884f4524e89efcd8a4df7a
souradeep Guha Neogi <39113709+sourad80@users.noreply.github.com>
2020-08-06 11:13:22 +0530

    Update mail_handler.php

--- mail_handler.php
+++ mail_handler.php
@@ -5,7 +5,7 @@
 		$subject=$_POST['subject'];
 		$msg=$_POST['msg'];
 
-		$to='xyz.123@mail.com'; // Receiver Email ID, Replace with your email ID
+		$to='souradee80@gmail.com'; // Receiver Email ID, Replace with your email ID
 		$message="Name :".$name."\n"."Wrote the following :"."\n\n".$msg;
 		$headers="From: ".$email;
 
